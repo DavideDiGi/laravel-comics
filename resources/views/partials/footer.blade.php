484827dbecc3cafbe4f7292df45948fcbbae5ef3
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-03-10 09:36:01 +0100

    aggiunto top-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,71 @@
 <footer class="bg-black">
 
+    <div class="middleFooter">
+
+        <div class="container">
+            <div class="links-container">
+
+                <div class="comics">
+
+                    <h3>DC COMICS</h3>
+
+                    <a href="#">Characters</a>
+                    <a href="#">Comics</a>
+                    <a href="#">Movies</a>
+                    <a href="#">TV</a>
+                    <a href="#">Games</a>
+                    <a href="#">Videos</a>
+                    <a href="#">News</a>
+
+                </div>
+                <div class="shop">
+
+                    <h3>SHOP</h3>
+
+                    <a href="#">Shop DC</a>
+                    <a href="#">Shop DC Collectibles</a>
+
+
+                </div>
+                <div class="dc">
+
+                    <h3>DC</h3>
+
+                    <a href="#">Terms Of Use</a>
+                    <a href="#">Privacy Policy (New)</a>
+                    <a href="#">Ad choices</a>
+                    <a href="#">Advertising</a>
+                    <a href="#">Jobs</a>
+                    <a href="#">Subscriptions</a>
+                    <a href="#">Talent Workshops</a>
+                    <a href="#">CPSC Certificates</a>
+                    <a href="#">Ratings</a>
+                    <a href="#">Shop Help</a>
+                    <a href="#">Contact Us</a>
+
+                </div>
+                <div class="sites">
+
+                    <h3>SITES</h3>
+
+                    <a href="#">DC</a>
+                    <a href="#">MAD Magazine</a>
+                    <a href="#">DC Kids</a>
+                    <a href="#">DC Universe</a>
+                    <a href="#">DC Power Visa</a>
+
+                </div>
+                
+                
+            </div>
+            
+            <div class="bg-logo-container"></div>
+            {{-- <div class="rights">All Site Content TM and c 2020 DC Entertainment, unless otherwise noted here. All rights reserved. Cookies Settings</div> --}}
+
+
+        </div>
+
+    </div>
     <div class="container">
        
         <div class="footer-container d-flex justify-content-between align-items-center">
